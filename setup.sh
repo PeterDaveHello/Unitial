a8f79b3b679c56e114e92931f3dcc9e9374d733d
Peter Dave Hello <hsu@peterdavehello.org>
2013-06-14 11:55:15 +0800

    try to fix ls color issue in Linux, and recognize Linux instead of FreeBSD when downloading files.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -x
 
 os=`uname`
-if [ $os = "FreeBSD" ]
+if [ $os = "Linux" ]
 then
-download='fetch'
-download_o='fetch -o'
-else
 download='wget -nv'
 download_o='wget -nv -O'
+else
+download='fetch'
+download_o='fetch -o'
 fi
 
 path='https://raw.github.com/PeterDaveHello/Unitial/master/'
@@ -19,6 +19,12 @@ do
     \rm "$files"
 done
 
+if [ $os = "Linux" ]
+then
+echo "alias ls='\ls -GF --autocolor'" >> ~/.bash_profile
+echo "alias ls    '\ls -GF --autocolor'" >> ~/.tcshrc
+fi
+
 mkdir -p ~/.vim/colors/
-${download_o} ~/.vim/colors/kolor.vim https://raw.github.com/zeis/vim-kolor/master/colors/kolor.vim 
+${download_o} ~/.vim/colors/kolor.vim https://raw.github.com/zeis/vim-kolor/master/colors/kolor.vim
 ${download_o} ~/.git-completion.bash https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
