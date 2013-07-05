b79dd759bab216a6b2bc358ba7321f3da51bb73f
Peter Dave Hello <hsu@peterdavehello.org>
2013-07-05 21:44:43 +0800

    Add more rompt message!

--- setup.sh
+++ setup.sh
@@ -21,6 +21,7 @@ fi
 
 path='https://raw.github.com/PeterDaveHello/Unitial/master/'
 
+${ECHO} -e "\n\e[1;36;40mTyy to download and setup configs from server...\n\e[0m";
 for files in gitconfig tcshrc bash_profile inputrc vimrc
 do
     ${download} "$path""$files"
@@ -28,6 +29,7 @@ do
     ${RM} "$files"
 done
 
+${ECHO} -e "\n\e[1;36;40mAdd some color setting which depends on your OS...\n\e[0m";
 if [ $os = "Linux" ]
 then
     ${ECHO} "alias ls='\ls -GF --color=auto'" >> ~/.bash_profile
@@ -36,8 +38,10 @@ else
     ${ECHO} "export LSCOLORS=gxfxcxdxbxegedabagacad" >> ~/.bash_profile
 fi
 
+${ECHO} -e "\n\e[1;36;40mTyy to download VIM color scheme from server...\n\e[0m";
 ${MKDIR} -p ~/.vim/colors/
 ${download_o} ~/.vim/colors/kolor.vim https://raw.github.com/zeis/vim-kolor/master/colors/kolor.vim
+${ECHO} -e "\n\e[1;36;40mTyy to download git's auto completion configs from server...\n\e[0m";
 ${download_o} ~/.git-completion.bash https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
 
 ${ECHO} -e "\n\e[1;36;40mUnitial is finished!\n\nPlease terminate all other works and restart your shell or re-login.\n \e[0m";
