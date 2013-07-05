8437a534224631caba16c3853929d514d212cdc1
Peter Dave Hello <hsu@peterdavehello.org>
2013-07-05 21:37:13 +0800

    add more message!

--- setup.sh
+++ setup.sh
@@ -5,12 +5,16 @@ ECHO="/bin/echo"
 MKDIR="/bin/mkdir"
 RM="/bin/rm"
 
+${ECHO} -e "\n\e[1;36;40mUnitial is started to initial your Unix-like working environment\n\nPlease wait...\n\n \e[0m";
+
 os=`uname`
 if [ $os = "Linux" ]
 then
+    ${ECHO} -e "\n\e[1;36;40mYour operating system is GNU/Linux\n\e[0m";
     download='wget -nv'
     download_o='wget -nv -O'
 else
+    ${ECHO} -e "\n\e[1;36;40mYour operating system is NOT GNU/Linux\nmaybe a BSD system!?\n\e[0m";
     download='fetch'
     download_o='fetch -o'
 fi
