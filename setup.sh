12a67ff9b70483a079a5f6693ac7f6f743bba364
Peter Dave Hello <hsu@peterdavehello.org>
2013-12-11 12:31:53 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,20 +5,21 @@ MKDIR="/bin/mkdir"
 RM="/bin/rm"
 
 os=`uname`
-if [ $os = "Linux" ]
-then
+if [ $os = "Linux" ] || [ $os = "GNU" ] || [ $os = "Darwin" ];then
     ECHO="/bin/echo"
-    ${ECHO} -e "\n\e[1;36;40mYour operating system is GNU/Linux\n\e[0m";
+    ${ECHO} -e "\n\e[1;36;40mYour operating system is $os\n\e[0m";
+    ${ECHO} -e "\n\e[1;36;40mSuppose you have 'wget' to download files!\n\e[0m";
     download='wget -nv --no-check-certificate'
     download_o='wget -nv --no-check-certificate -O'
 else
     ECHO="echo"
-    ${ECHO} -e "\n\e[1;36;40mYour operating system is NOT GNU/Linux, maybe a BSD system!?\n\e[0m";
+    ${ECHO} -e "\n\e[1;36;40mYour operating system is $os\n\e[0m";
+    ${ECHO} -e "\n\e[1;36;40mSuppose you are using a BSD system and you have 'fetch' to download files!\n\e[0m";
     download='fetch'
     download_o='fetch -o'
 fi
 
-${ECHO} -e "\n\e[1;36;40mUnitial is started to initial your Unix-like working environment\n\nPlease wait...\n\n \e[0m";
+${ECHO} -e "\n\e[1;36;40mUnitial is started to initial your Unix-like working environment\n\nPlease wait...\n\n\e[0m";
 path='https://raw.github.com/PeterDaveHello/Unitial/master/'
 
 ${ECHO} -e "\n\e[1;36;40mTry to download and setup configs from server...\n\e[0m";
@@ -30,15 +31,14 @@ do
 done
 
 ${ECHO} -e "\n\e[1;36;40mAdd some color setting which depends on your OS...\n\e[0m";
-if [ $os = "Linux" ]
-then
-    ${ECHO} "alias ls='\ls -F --color=auto'" >> ~/.bash_profile
-    ${ECHO} "alias ls='\ls -F --color=auto'" >> ~/.zshrc
-    ${ECHO} "alias ls '\ls -F --color=auto'" >> ~/.tcshrc
-else
-    ${ECHO} "alias ls='\ls -GF'" >> ~/.bash_profile
-    ${ECHO} "alias ls='\ls -GF'" >> ~/.zshrc
-    ${ECHO} "alias ls '\ls -GF'" >> ~/.tcshrc
+if [ $os = "Linux" ] || [ $os = "GNU" ];then
+    ${ECHO} -e "\n#color setting\nalias ls='\ls -F --color=auto'" >> ~/.bash_profile
+    ${ECHO} -e "\n#color setting\nalias ls='\ls -F --color=auto'" >> ~/.zshrc
+    ${ECHO} -e "\n#color setting\nalias ls '\ls -F --color=auto'" >> ~/.tcshrc
+elif [ $os = "FreeBSD" ] || [ $os = "Darwin" ] || [ $os = "DragonFly" ] || [ $os = "OpenBSD" ];then
+    ${ECHO} -e "\n#color setting\nalias ls='\ls -GF'" >> ~/.bash_profile
+    ${ECHO} -e "\n#color setting\nalias ls='\ls -GF'" >> ~/.zshrc
+    ${ECHO} -e "\n#color setting\nalias ls '\ls -GF'" >> ~/.tcshrc
     ${ECHO} "export LSCOLORS=gxfxcxdxbxegedabagacad" >> ~/.bash_profile
 fi
 
@@ -50,4 +50,4 @@ git_auto_complete_path='https://raw.github.com/git/git/master/contrib/completion
 ${download_o} ~/.git-completion.bash "$git_auto_complete_path"bash
 ${download_o} ~/.git-completion.tcsh "$git_auto_complete_path"tcsh
 
-${ECHO} -e "\n\e[1;36;40mUnitial is finished!\n\nPlease terminate all other works and restart your shell or re-login.\n \e[0m";
+${ECHO} -e "\n\e[1;36;40mUnitial installation was finished!\n\nPlease terminate all other works and restart your shell or re-login.\n\e[0m";
