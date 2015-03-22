PeterDaveHello/Unitial

commit 256b85ab8c1b0e16aee08c787a0b2f8a6eaa7774

bashrc: force enable color prompt for a short time

bashrc
@@ -75,7 +75,7 @@ if [ -e ~/.bash_history ]; then
 # network-related commands or tools which need auto completion
     complete -W "$(echo $(grep '^ssh ' ~/.bash_history | sort -u | sed 's/^ssh //'))" ssh
     complete -W "$(echo $(grep '^sftp ' ~/.bash_history | sort -u | sed 's/^sftp //'))" sftp
-    complete -W "$(echo $(grep '^ping ' ~/.bash_history | sort -u | sed 's/^ping //'))" ping
+    complete -W "$(echo $(guse_colorrep '^ping ' ~/.bash_history | sort -u | sed 's/^ping //'))" ping
     complete -W "$(echo $(grep '^dig ' ~/.bash_history | sort -u | sed 's/^dig //'))" dig
     complete -W "$(echo $(grep '^telnet ' ~/.bash_history | sort -u | sed 's/^telnet //'))" telnet
     complete -W "$(echo $(grep '^mosh ' ~/.bash_history | sort -u | sed 's/^mosh //'))" mosh
@@ -88,7 +88,7 @@ if [ -e ~/.git-completion.bash ]; then
 fi
 
 # bashrc ref to linuxmint 17.1's default /etc/bash.bashrc
-use_color=false
+use_color=true #'cause this detection not work on FreeBSD(10.1), force enable for a short time
 safe_term=${TERM//[^[:alnum:]]/?}   # sanitize TERM
 match_lhs=""
 

commit 670d5601c80408017f897299bc43fa9acb05faca

bashrc: fix typo caused in 256b85ab

bashrc
@@ -75,7 +75,7 @@ if [ -e ~/.bash_history ]; then
 # network-related commands or tools which need auto completion
     complete -W "$(echo $(grep '^ssh ' ~/.bash_history | sort -u | sed 's/^ssh //'))" ssh
     complete -W "$(echo $(grep '^sftp ' ~/.bash_history | sort -u | sed 's/^sftp //'))" sftp
-    complete -W "$(echo $(guse_colorrep '^ping ' ~/.bash_history | sort -u | sed 's/^ping //'))" ping
+    complete -W "$(echo $(grep '^ping ' ~/.bash_history | sort -u | sed 's/^ping //'))" ping
     complete -W "$(echo $(grep '^dig ' ~/.bash_history | sort -u | sed 's/^dig //'))" dig
     complete -W "$(echo $(grep '^telnet ' ~/.bash_history | sort -u | sed 's/^telnet //'))" telnet
     complete -W "$(echo $(grep '^mosh ' ~/.bash_history | sort -u | sed 's/^mosh //'))" mosh